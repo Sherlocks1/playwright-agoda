0d3c8fe168edf28f968889720dc4139103f40996
Sherlocks1 <510154365@qq.com>
2023-03-15 00:22:43 +0800

    调试bug

--- mixgetdata.py
+++ mixgetdata.py
@@ -17,13 +17,20 @@ def clean_filename(url: str) -> str:
 async def get_data(page, url, filenames, max_retries=4):
     page1 = None
     retries = 0
+
     while True:
         try:
 
             await page.goto(url)
 
-            # 点击AGD-logo
-            await page.locator("div[class='css-170e3kd e19j9nj21'] img[alt='AGD-logo']").click()
+            page.set_default_timeout(10000)
+
+            # 等待 AGD-logo 出现
+            agd_logo = await page.wait_for_selector("div[class='css-170e3kd e19j9nj21'] img[alt='AGD-logo']",
+                                                    timeout=10000)
+
+            # 单击 AGD-logo
+            await agd_logo.click()
 
             # 等待新窗口打开
             async with page.expect_popup() as page1_info:
@@ -48,9 +55,14 @@ async def get_data(page, url, filenames, max_retries=4):
         while True:
             try:
 
+                # 等待链接出现
+                link = await page1.wait_for_selector(
+                    'xpath=/html[1]/body[1]/div[11]/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/div[3]/ol[1]/li[1]/div['
+                    '2]/a[1]/div[1]/div[3]/div[1]',
+                    timeout=10000)
+
                 # 单击链接
-                await page1.locator('xpath=/html[1]/body[1]/div[11]/div[1]/div[3]/div[1]/div[1]/div[2]/div[1]/div['
-                                    '3]/ol[1]/li[1]/div[2]/a[1]/div[1]/div[3]/div[1]').click()
+                await link.click()
 
                 page2 = await page2_info.value
                 await page1.close()
@@ -72,7 +84,7 @@ async def get_data(page, url, filenames, max_retries=4):
         try:
 
             # 等待元素加载完成
-            await page2.wait_for_selector("//*[@id='roomGrid']", timeout=90000)
+            await page2.wait_for_selector("//*[@id='roomGrid']", timeout=10000)
 
             html = await page2.content()
             filename = clean_filename(url)
@@ -113,7 +125,7 @@ async def main():
         context = await browser.new_context()
 
         # 设置页面默认超时时间为60秒
-        timeout = 90 * 1000  # 90 minutes in milliseconds
+        timeout = 180 * 1000  # 90 minutes in milliseconds
         context.set_default_navigation_timeout(timeout)
 
         tasks = []
